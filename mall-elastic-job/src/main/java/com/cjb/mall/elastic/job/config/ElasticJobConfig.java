9a88dd1ddc87f59f7e77e1180af110b904830c38
‘lisi’ <4814556@qq.com>
2020-03-17 09:18:44 +0800

    修改

--- mall-elastic-job/src/main/java/com/cjb/mall/elastic/job/config/ElasticJobConfig.java
+++ mall-elastic-job/src/main/java/com/cjb/mall/elastic/job/config/ElasticJobConfig.java
@@ -1,38 +1,38 @@
-package com.cjb.mall.elastic.job.config;
-
-import com.cjb.mall.elastic.job.task.Job1;
-import com.dangdang.ddframe.job.api.simple.SimpleJob;
-import com.dangdang.ddframe.job.config.JobCoreConfiguration;
-import com.dangdang.ddframe.job.config.dataflow.DataflowJobConfiguration;
-import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
-import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
-import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
-import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ElasticJobConfig {
-
-    @Autowired
-    SimpleJob job1;
-
-    @Autowired
-    CoordinatorRegistryCenter registryCenter;
-
-    private LiteJobConfiguration createJobConfiguration(){
-        // 创建作业配置
-        JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("job1", "0/3 * * * * ?", 1).build();
-        SimpleJobConfiguration dataflowJobConfig = new SimpleJobConfiguration(coreConfig, Job1.class.getCanonicalName());
-        LiteJobConfiguration result = LiteJobConfiguration.newBuilder(dataflowJobConfig).overwrite(true).build();
-        return result;
-    }
-
-    @Bean(initMethod = "init")
-    public SpringJobScheduler initSimpleElasticJob(){
-        SpringJobScheduler springJobScheduler = new SpringJobScheduler(job1, registryCenter, createJobConfiguration());
-        return springJobScheduler;
-
-    }
-}
+//package com.cjb.mall.elastic.job.config;
+//
+//import com.cjb.mall.elastic.job.task.Job1;
+//import com.dangdang.ddframe.job.api.simple.SimpleJob;
+//import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+//import com.dangdang.ddframe.job.config.dataflow.DataflowJobConfiguration;
+//import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
+//import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
+//import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
+//import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ElasticJobConfig {
+//
+//    @Autowired
+//    SimpleJob job1;
+//
+//    @Autowired
+//    CoordinatorRegistryCenter registryCenter;
+//
+//    private LiteJobConfiguration createJobConfiguration(){
+//        // 创建作业配置
+//        JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("job1", "0/3 * * * * ?", 3).build();
+//        SimpleJobConfiguration dataflowJobConfig = new SimpleJobConfiguration(coreConfig, Job1.class.getCanonicalName());
+//        LiteJobConfiguration result = LiteJobConfiguration.newBuilder(dataflowJobConfig).overwrite(true).build();
+//        return result;
+//    }
+//
+//    @Bean(initMethod = "init")
+//    public SpringJobScheduler initSimpleElasticJob(){
+//        SpringJobScheduler springJobScheduler = new SpringJobScheduler(job1, registryCenter, createJobConfiguration());
+//        return springJobScheduler;
+//
+//    }
+//}
--- mall-elastic-job/src/main/java/com/cjb/mall/elastic/job/task/Job1.java
+++ mall-elastic-job/src/main/java/com/cjb/mall/elastic/job/task/Job1.java
@@ -1,17 +1,17 @@
-package com.cjb.mall.elastic.job.task;
-
-import com.dangdang.ddframe.job.api.ShardingContext;
-import com.dangdang.ddframe.job.api.simple.SimpleJob;
-import org.springframework.stereotype.Component;
-
-import java.util.UUID;
-
-@Component
-public class Job1 implements SimpleJob {
-
-    //任务执行的代码逻辑
-    @Override
-    public void execute(ShardingContext shardingContext) {
-        System.out.println(UUID.randomUUID().toString());
-    }
-}
+//package com.cjb.mall.elastic.job.task;
+//
+//import com.dangdang.ddframe.job.api.ShardingContext;
+//import com.dangdang.ddframe.job.api.simple.SimpleJob;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.UUID;
+//
+//@Component
+//public class Job1 implements SimpleJob {
+//
+//    //任务执行的代码逻辑
+//    @Override
+//    public void execute(ShardingContext shardingContext) {
+//        System.out.println(UUID.randomUUID().toString());
+//    }
+//}
